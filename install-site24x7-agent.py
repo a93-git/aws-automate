dbc108d83fd913d5e62358bb031f52f38e302377
Kishore Sharma <abhishek.sharma@softwareone.com>
2020-02-05 17:08:36 +0530

    Add ssm status check function

--- install-site24x7-agent.py
+++ install-site24x7-agent.py
@@ -82,6 +82,15 @@ class Site24x7Installer():
             }
         )
 
+    def check_ssm_status(self, instance_id):
+        try:
+            a = self.client_ssm.describe_instance_information(Filters=[{'Key':'InstanceIds', 'Values': [instance_id]}])['InstanceInformationList'][0]['PingStatus']
+            logger.info("Ping status is {0}".format(a))
+            return True
+        except Exception as e:
+            logger.info("SSM agent is not installed. Error message is {0}".format(e))
+            return False
+
     def install_agent(self):
         ec2_matching = []
         for reservation in self.ec2_data['Reservations']:
@@ -101,14 +110,20 @@ class Site24x7Installer():
                         # No need to check if the service is already running because if it is, the script won't proceed with installation
                         command = "If (Get-Service '" + service_name + "' ) { Restart-Service -Name '" + service_name + "' } Else { echo 'we are in the else block'; Start-Job -Name WebReq -ScriptBlock { Invoke-WebRequest -Uri " + url + " -OutFile " + output + " }; Wait-Job -Name WebReq; Test-Path " + output + " -PathType Leaf; msiexec.exe /i " + output + " EDITA1=" + self.activation_key + " ENABLESILENT=YES REBOOT=ReallySuppress /qn }"
                         instance_id = x[0]
-                        self.send_command(instance_id, command, document_name, self.output_s3_key_prefix_windows)
+                        if self.check_ssm_status(instance_id):
+                            self.send_command(instance_id, command, document_name, self.output_s3_key_prefix_windows)
+                        else:
+                            logger.info("Skipping instance id {0} as SSM agent is not installed".format(instance_id))
                 else:
                     logger.info("EC2 instance {0} is in {1} state".format(x[0], x[2]))
             elif x[1] == 'running':
                 document_name = 'AWS-RunShellScript'
                 command = 'a=$(/etc/init.d/site24x7monagent status | head -1); if [[ $a == "Site24x7 monitoring agent service is up" ]]; then echo "Agent is already installed" > /var/log/test.log; /etc/init.d/site24x7monagent restart; else bash -c "$(curl -sL https://staticdownloads.site24x7.eu/server/Site24x7InstallScript.sh)" readlink -i -key=' + self.activation_key + ' ; fi'
                 instance_id = x[0]
-                self.send_command(instance_id, command, document_name, self.output_s3_key_prefix_linux)
+                if self.check_ssm_status(instance_id):
+                    self.send_command(instance_id, command, document_name, self.output_s3_key_prefix_linux)
+                else:
+                    logger.info("Skipping instance id {0} as SSM agent is not installed".format(instance_id))
             else:
                 logger.info("EC2 instance {0} is in {1} state".format(x[0], x[1]))
 
