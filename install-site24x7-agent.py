09df1dc58ea4afd84bba7fb3e5d2d7a8153e86be
a93-git <42908045+a93-git@users.noreply.github.com>
2020-02-11 22:09:03 +0530

    Installation works now

--- install-site24x7-agent.py
+++ install-site24x7-agent.py
@@ -9,16 +9,19 @@ Tag_Value - Tag value for the corresponding Tag_Key
 Output_S3_Bucket - S3 bucket to store the output of the SSM command
 Output_S3_Key_Prefix_Windows - S3 bucket folder name inside the above bucket for Windows instances
 Output_S3_Key_Prefix_Linux - S3 bucket folder name inside the above bucket for Windows instances
-Service_Role_Arn - ARN of the role to allow the SNS topic in the SSM command to send the notification
-Notification_Arn - ARN of the SNS topic that will send the notification (failure or success of the SSM command)
+SNS_Topic - ARN of the SNS topic that will send the notification (failure or success of the SSM command)
 CW_Log_Group_Name - Cloudwatch log group name to save the output of the Lambda function
 """
+
 import boto3
 import os
 import logging
+import datetime
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+message = {}
 
 REGION_NAMES = ['Ohio', 'N. Virginia', 'N. California', 'Oregon', 'Mumbai', 'Osaka-Local', 'Seoul', 'Singapore', 'Sydney', 'Tokyo', 'Canada', 'Frankfurt', 'Ireland', 'London', 'Paris', 'Stockholm', 'Sao Paulo']
 REGION_CODES = ['us-east-2', 'us-east-1', 'us-west-1', 'us-west-2', 'ap-south-1', 'ap-northeast-3', 'ap-northeast-2', 'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'ca-central-1', 'eu-central-1', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'eu-north-1', 'sa-east-1']
@@ -33,13 +36,12 @@ class Site24x7Installer():
     output_s3_bucket_name = os.environ['Output_S3_Bucket']
     output_s3_key_prefix_linux = os.environ['Output_S3_Key_Prefix_Linux']
     output_s3_key_prefix_windows = os.environ['Output_S3_Key_Prefix_Windows']
-    service_role_arn = os.environ['Service_Role_Arn']
-    notification_arn = os.environ['Notification_Arn']
     cw_log_group_name = os.environ['CW_Log_Group_Name']
 
     def __init__(self, region):
         """ Instantiate the class, create ssm and ec2 clients and fetch the ec2 data for given tag keys and values
         """
+        self.region = region
         self.client_ssm = boto3.client('ssm', region_name=region)
         self.client_ec2 = boto3.client('ec2', region_name=region)
 
@@ -54,44 +56,47 @@ class Site24x7Installer():
         )
 
     def send_command(self, instance_id, command, document_name, output_s3_key_prefix):
-        self.client_ssm.send_command(
-            Targets=[
-                {
-                    'Key': 'InstanceIds',
-                    'Values': [
-                        instance_id]
+        try:
+            retval = self.client_ssm.send_command(
+                Targets=[
+                    {
+                        'Key': 'InstanceIds',
+                        'Values': [
+                            instance_id]
+                    }
+                ],
+                DocumentName=document_name,
+                DocumentVersion=self.document_version,
+                TimeoutSeconds=self.timeout_seconds,
+                Parameters={
+                    'commands': [command]
+                },
+                OutputS3BucketName=self.output_s3_bucket_name,
+                OutputS3KeyPrefix=output_s3_key_prefix,
+                CloudWatchOutputConfig={
+                    'CloudWatchLogGroupName': self.cw_log_group_name,
+                    'CloudWatchOutputEnabled': True
                 }
-            ],
-            DocumentName=document_name,
-            DocumentVersion=self.document_version,
-            TimeoutSeconds=self.timeout_seconds,
-            Parameters={
-                'commands': [command]
-            },
-            OutputS3BucketName=self.output_s3_bucket_name,
-            OutputS3KeyPrefix=output_s3_key_prefix,
-            ServiceRoleArn=self.service_role_arn,
-            NotificationConfig={
-                'NotificationArn': self.notification_arn,
-                'NotificationEvents': ['Success', 'TimedOut', 'Failed'],
-                'NotificationType': 'Invocation'
-            },
-            CloudWatchOutputConfig={
-                'CloudWatchLogGroupName': self.cw_log_group_name,
-                'CloudWatchOutputEnabled': True
-            }
-        )
+            )
+            
+            return retval
+            
+        except Exception as e:
+            logger.info("Error in installing Site24x7 agent on instance id {0} in region {1}".format(instance_id, self.region))
+            logger.error(e)
+            return False
 
     def check_ssm_status(self, instance_id):
         try:
             a = self.client_ssm.describe_instance_information(Filters=[{'Key':'InstanceIds', 'Values': [instance_id]}])['InstanceInformationList'][0]['PingStatus']
-            logger.info("Ping status is {0}".format(a))
+            logger.info("Ping status is {0} for instance {1} in region {2}".format(a, instance_id, self.region))
             return True
         except Exception as e:
-            logger.info("SSM agent is not installed. Error message is {0}".format(e))
+            logger.info("SSM agent is not installed on instance {0} in region {1}. Error message is {2}".format(instance_id, self.region, e))
             return False
 
     def install_agent(self):
+        message[self.region] = []
         ec2_matching = []
         for reservation in self.ec2_data['Reservations']:
             for instance in reservation['Instances']:
@@ -111,23 +116,61 @@ class Site24x7Installer():
                         command = "If (Get-Service '" + service_name + "' ) { Restart-Service -Name '" + service_name + "' } Else { echo 'we are in the else block'; Start-Job -Name WebReq -ScriptBlock { Invoke-WebRequest -Uri " + url + " -OutFile " + output + " }; Wait-Job -Name WebReq; Test-Path " + output + " -PathType Leaf; msiexec.exe /i " + output + " EDITA1=" + self.activation_key + " ENABLESILENT=YES REBOOT=ReallySuppress /qn }"
                         instance_id = x[0]
                         if self.check_ssm_status(instance_id):
-                            self.send_command(instance_id, command, document_name, self.output_s3_key_prefix_windows)
+                            retval = self.send_command(instance_id, command, document_name, self.output_s3_key_prefix_windows)
+                            if retval:
+                                message[self.region].append({instance_id: retval['Command']['CommandId']})
+                            else:
+                                message[self.region].append({instance_id: "Error in installing SSM agent"})
                         else:
                             logger.info("Skipping instance id {0} as SSM agent is not installed".format(instance_id))
+                            message[self.region].append({instance_id: "SSM agent not installed"})
                 else:
                     logger.info("EC2 instance {0} is in {1} state".format(x[0], x[2]))
+                    message[self.region].append({x[0]: "EC2 instance in {0} state".format(x[2])})
             elif x[1] == 'running':
                 document_name = 'AWS-RunShellScript'
                 command = 'a=$(/etc/init.d/site24x7monagent status | head -1); if [[ $a == "Site24x7 monitoring agent service is up" ]]; then echo "Agent is already installed" > /var/log/test.log; /etc/init.d/site24x7monagent restart; else bash -c "$(curl -sL https://staticdownloads.site24x7.eu/server/Site24x7InstallScript.sh)" readlink -i -key=' + self.activation_key + ' ; fi'
                 instance_id = x[0]
                 if self.check_ssm_status(instance_id):
-                    self.send_command(instance_id, command, document_name, self.output_s3_key_prefix_linux)
+                    retval = self.send_command(instance_id, command, document_name, self.output_s3_key_prefix_linux)
+                    if retval:
+                        message[self.region].append({instance_id: retval['Command']['CommandId']})
+                    else:
+                        message[self.region].append({instance_id: "Error in installing SSM agent"})
                 else:
-                    logger.info("Skipping instance id {0} as SSM agent is not installed".format(instance_id))
+                    logger.info("Skipping instance id {0} in region {1} as SSM agent is not installed".format(instance_id, self.region))
+                    message[self.region].append({instance_id: "SSM agent not installed"})
             else:
-                logger.info("EC2 instance {0} is in {1} state".format(x[0], x[1]))
+                logger.info("EC2 instance {0} in region {1} is in {2} state".format(x[0], self.region, x[1]))
+                message[self.region].append({x[0]: "EC2 instance in {0} state".format(x[1])})
+
+def send_notification(message):
+    """ Send SNS notification"""
+    
+    client_session = boto3.session.Session()
+    region = client_session.region_name
+    
+    client_sns = boto3.client('sns', region_name=region)
+    
+    sns_arn = os.environ['SNS_Topic']
+    
+    message_id = client_sns.publish(
+        TopicArn=sns_arn,
+        Message= repr(message),
+        Subject = "Site24x7 agent installation details on {0}".format(datetime.datetime.now().strftime("%Y-%m-%d"))
+        )
+        
+    return message_id
 
 def lambda_handler(event, context):
     for region in REGIONS.values():
-        a = Site24x7Installer(region)
-        a.install_agent()
+        try:
+            a = Site24x7Installer(region)
+            a.install_agent()
+        except Exception as e:
+            logger.info("Error in installing Site24x7 agent in region {0}".format(region))
+            logger.error(e)
+            message[region] = "Error occured in installing Site24x7 agents in this region"
+            
+    message_id = send_notification(message)
+    logger.info("Message ID is {0}".format(message_id['MessageId']))
