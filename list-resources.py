0a857f560d0cd859b77f9d50c922cf2018064a05
a93-git <42908045+a93-git@users.noreply.github.com>
2020-02-13 20:23:13 +0530

    Get EC2 data for all regions

--- list-resources.py
+++ list-resources.py
@@ -2,60 +2,109 @@
 
 import boto3
 import json
-import pprint
 
 REGION_NAMES = ['Ohio', 'N. Virginia', 'N. California', 'Oregon', 'Mumbai', 'Osaka-Local', 'Seoul', 'Singapore', 'Sydney', 'Tokyo', 'Canada', 'Frankfurt', 'Ireland', 'London', 'Paris', 'Stockholm', 'Sao Paulo']
 REGION_CODES = ['us-east-2', 'us-east-1', 'us-west-1', 'us-west-2', 'ap-south-1', 'ap-northeast-3', 'ap-northeast-2', 'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'ca-central-1', 'eu-central-1', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'eu-north-1', 'sa-east-1']
 REGIONS = dict(zip(REGION_NAMES, REGION_CODES))
 
-def get_ec2_info(client_ec2):
-    """ Retrieve the information about the EC2 instances in a given region
+class EC2Info():
+    """Create an object to get EC2 data"""
 
-    Arguments:
-    client_ec2 - A boto3 client to the ec2 resource for a particular region
+    def __init__(self):
+        """ Create the client and initialize the object """
+        self.instance_info = {}
 
-    Returns:
-    A json object with the information about the instances
+    def get_data(self, client_ec2, region):
+        """ Retrieve the information about the EC2 instances in a given region
 
-    Return structure:
-    {
-        'Instances':
-            {
-                'InstanceId':
-                {
-                    'Platform': '',
-                    'PublicIpAddress': '',
-                    'PrivateIpAddress': '',
-                    'InstanceType': '',
-                    'InstanceState': ''
+        Arguments:
+        client_ec2 - boto3 client to AWS ec2 service for the given region
+
+        Returns:
+        A json object with the information about the instances
+
+        Return structure:
+        {
+            Region: {
+                'Instances': {
+                    'InstanceId': {
+                        'Platform': '',
+                        'PublicIpAddress': '',
+                        'PrivateIpAddress': '',
+                        'InstanceType': '',
+                        'InstanceState': ''
+                    },
+                    ...
+                }
+            }
+        }
+
+        """
+
+        retval = {'Instances': {}}
+        ec2_list = client_ec2.describe_instances()
+        # self.instance_info[region] = {}
+        for reservation in ec2_list['Reservations']:
+            for instance in reservation['Instances']:
+                retval['Instances'][instance['InstanceId']] = {}
+                if 'Platform' in instance.keys():
+                    retval['Instances'][instance['InstanceId']]['Platform'] = instance['Platform']
+                else:
+                    retval['Instances'][instance['InstanceId']]['Platform'] = "None"
+                if 'PublicIpAddress' in instance.keys():
+                    retval['Instances'][instance['InstanceId']]['PublicIpAddress'] = instance['PublicIpAddress']
+                else:
+                    retval['Instances'][instance['InstanceId']]['PublicIpAddress'] = "None"
+                retval['Instances'][instance['InstanceId']]['InstanceState'] = instance['State']['Name']
+                retval['Instances'][instance['InstanceId']]['InstanceType'] = instance['InstanceType']
+                retval['Instances'][instance['InstanceId']]['PrivateIpAddress'] = instance['PrivateIpAddress']
+        return retval
+
+    def get_ec2_info(self):
+        """ Loop over all the available regions and get the EC2 info
+        
+        Arguments:
+        None
+        
+        Return:
+        A json object of the regions, instances and their values
+        
+        Return Structure:
+        
+        {
+            'Region1': {
+                'Instances': {
+                    'InstanceId': {
+                        'Platform': '',
+                        'PublicIpAddress': '',
+                        'PrivateIpAddress': '',
+                        'InstanceType': '',
+                        'InstanceState': ''
+                    },
+                    ...
                 },
                 ...
             }
-    }
+        }
+        
+        """
 
-    """
-    instance_info = {}
-    ec2_list = client_ec2.describe_instances()
-    instance_info['Instances'] = {}
-    for reservation in ec2_list['Reservations']:
-        for instance in reservation['Instances']:
-            instance_info['Instances'][instance['InstanceId']] = {}
-            if 'Platform' in instance.keys():
-                instance_info['Instances'][instance['InstanceId']]['Platform'] = instance['Platform']
-            else:
-                instance_info['Instances'][instance['InstanceId']]['Platform'] = "None"
-            if 'PublicIpAddress' in instance.keys():
-                instance_info['Instances'][instance['InstanceId']]['PublicIpAddress'] = instance['PublicIpAddress']
-            else:
-                instance_info['Instances'][instance['InstanceId']]['PublicIpAddress'] = "None"
-            instance_info['Instances'][instance['InstanceId']]['InstanceState'] = instance['State']['Name']
-            instance_info['Instances'][instance['InstanceId']]['InstanceType'] = instance['InstanceType']
-            instance_info['Instances'][instance['InstanceId']]['PrivateIpAddress'] = instance['PrivateIpAddress']
+        for region in REGIONS.values():
+            self.instance_info[region] = {}
+            try:
+                client_ec2 = boto3.client('ec2', region_name=region)
+                retval = self.get_data(client_ec2, region)
+                self.instance_info[region] = retval
+            except boto3.exceptions.botocore.client.ClientError as e:
+                print(e)
+            except:
+                raise
 
-    return json.dumps(instance_info)
-    #        print(instance['InstanceId'], instance['InstanceType'], instance['Platform'], instance['PrivateIpAddress'], instance['PublicIpAddress'], instance['State'])
+        return json.dumps(self.instance_info, indent=4)
 
 if __name__ == '__main__':
-    client_ec2 = boto3.client('ec2', region_name='ap-south-1')
-    retval = get_ec2_info(client_ec2)
-    print(retval)
+    ec2_obj = EC2Info()
+    retval = ec2_obj.get_ec2_info()
+
+    with open('testec2info.json', 'w') as f:
+        f.write(retval)
